dca42ac438d21fae66f486c1d43c9d0ea1c45328
Homayoun <homayoun763@gmail.com>
2025-06-25 23:56:05 +0330

    Main Script Setup

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     let nav = document.querySelector('nav');
     console.log(nav)
+    nav.innerHTML = `
+        <h2>Hello DOM</h2>
+        <p>This is HTML within a Javascript string</p>
+    ` 
 })
