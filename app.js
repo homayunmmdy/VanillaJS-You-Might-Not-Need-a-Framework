26707d7d25471b9296d6c6aae7d17825b3099f29
Homayoun <homayoun763@gmail.com>
2025-06-29 19:29:12 +0330

    helpful shorthand methods

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-    let nav = document.querySelector('nav');
+    let nav = $('nav');
     console.log(nav)
     nav.innerHTML = `
         <h2>Hello DOM</h2>
         <p>This is HTML within a Javascript string</p>
     ` 
 })
+
+const $ = () => document.querySelector.call(this, arguments);
+const $$ = () => document.querySelectorAll.call(this, arguments);
+HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
+HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
+HTMLElement.prototype.$ = (s) => this.querySelector(s);
+HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
