0e96b978b4807a8c66f526ef39c0ad8549602421
Homayoun <homayoun763@gmail.com>
2025-07-04 20:33:15 +0330

    dynamic-routing

--- services/Router.js
+++ services/Router.js
@@ -8,6 +8,10 @@ const Router = {
         Router.go(url);
       });
     });
+    // Event Handler for URL changes
+    window.addEventListener('popstate', event => {
+      Router.go(event.state.route,false);
+    })
     // Checking the initial URL
     Router.go(location.pathname);
   },
@@ -27,6 +31,13 @@ const Router = {
         pageElement = document.createElement("h1");
         pageElement.textContent = "Your Order";
         break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Detail";
+          const paramId = route.substring(route.lastIndexOf('-')+1);
+          pageElement.dataset.id = paramId;
+        }
     }
 
     if (pageElement) {
