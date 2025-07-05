9c1e38e624dfd8313de3fb972d406c2318c400c4
Homayoun <homayoun763@gmail.com>
2025-07-06 00:06:30 +0330

    creating web components

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import API from './services/API.js';
 import { loadData } from './services/Menu.js';
 import Router from './services/Router.js';
 
+// Link my Web Components 
+import { MenuPage } from './components/MenuPage.js';
+import { OrderPage } from './components/OrderPage.js';
+import { DetailsPage } from './components/DetailsPage.js';
+
 window.app = {};
 app.store = Store;
 app.router = Router;
