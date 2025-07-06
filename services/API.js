aeb6210d62fdb93aaf416808e0bd38b0ad6634ee
Homayoun <homayoun763@gmail.com>
2025-07-06 13:29:28 +0330

    change link of link home

--- services/API.js
+++ services/API.js
@@ -1,6 +1,6 @@
 const API = {
-    // url: "/data/menu.json", // change to this to see in the local
-    url: "https://homayunmmdy.github.io/VanillaJS-You-Might-Not-Need-a-Framework/data/menu.json",
+    url: "/data/menu.json", // change to this to see in the local
+    // url: "https://homayunmmdy.github.io/VanillaJS-You-Might-Not-Need-a-Framework/data/menu.json",
     fetchMenu: async () => {
         const result = await fetch(API.url);
         return await result.json();
